56deacb84757febf08f09f444ff54d5b27470ab6
aliffikri21 <haykalalif21@gmail.com>
2025-05-29 16:12:35 +0800

    part 14 - n + 1 problem

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+    // $posts = Post::with(['author', 'category'])
+    //     ->latest()
+    //     ->get(); // Eager Loading
+    $posts = Post::latest()->get();
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
@@ -23,11 +27,13 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 });
 
 Route::get('/authors/{user:username}', function (User $user) {
+    // $posts = $user->posts->load('category', 'author'); // Eager Loading
     return view('posts', ['title' => count($user->posts) . ' Articles by ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', ['title' =>'Articles in: ' . $category->name, 'posts' => $category->posts]);
+    // $posts = $category->posts->load('category', 'author'); // Lazy Eager Loading
+    return view('posts', ['title' => 'Articles in: ' . $category->name, 'posts' => $category->posts]);
 });
 
 Route::get('/contact', function () {
